16cb8d60c0735c8c786ff3cd5896af6e7be0a0e0
LiyaM4 <amaliya1.am@gmail.comm>
2024-04-28 17:29:32 +0300

    last try

---
+++ les_3.py
@@ -0,0 +1,15 @@
+# Задание 1
+type = input("Введите вид питомца: ")
+nickname = input("Введите кличку питомца: ")
+age = input("Введите вораст питомца: ")
+print("Это " + type + " по кличке '" + nickname + "'. Возраст: " + age + " года.")
+
+
+# Задание 2
+a1 = "homo habilis"
+a2 = "homo erectus"
+a3 = "homo erectus"
+a4 = "homo heidelbergensis"
+a5 = "Homo sapiens neanderthalensis"
+a6 = "homo sapiens"
+print(a1, a2, a3, a4, a5, a6, sep="=>")
